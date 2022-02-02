9413a275d87e871a5038b37e43dec134c329b57b
Pijama17 <pijamafaze@gmail.com>
2022-02-02 14:10:21 +0100

    sql eindopdracht verbeterd wat ik kon

--- eindopdracht/antwoorden.sql
+++ eindopdracht/antwoorden.sql
@@ -1,31 +1,31 @@
 -- Opdracht 1 
-SELECT * FROM videogamesales;
+SELECT * FROM `videogamesales`
 -- Opdracht 2 
-SELECT SUM(year) AS uitgebracht_games_jaar1999 FROM videogamesales WHERE year = 1999;
+SELECT * FROM `videogamesales` WHERE year = 1999
 -- Opdracht 3
-SELECT SUM(NA_Sales) AS verkochte_sport_games_Noord_Amerika FROM videogamesales WHERE genre = "Sports";
+SELECT NA_Sales FROM `videogamesales` WHERE genre = 'sports' 
 -- Opdracht 4
-SELECT name, platform FROM videogamesales WHERE publisher = "Nintendo" AND year BETWEEN 1990 AND 2005;-- Opdracht 5
+SELECT name FROM `videogamesales` WHERE publisher = 'Nintendo' AND year >= 1990 AND year <= 2005
 -- Opdracht 5
-SELECT name, MAX(Global_Sales) AS hoogste_aantal_wereld FROM videogamesales;
-             ---Wii Sports---
+SELECT MAX(Global_Sales) , name AS Meeste_Sales_Wereldwijd FROM `videogamesales` 
 -- Opdracht 6 
-SELECT ROUND(AVG(EU_Sales)) AS gemiddeld_verkocht_eu FROM videogamesales WHERE genre = "Puzzle";
+SELECT AVG(Global_Sales) FROM `videogamesales` WHERE genre = 'puzzle'
 -- Opdracht 7 
-SELECT name, genre, publisher FROM videogamesales WHERE JP_Sales =532;
+SELECT name , genre , publisher FROM `videogamesales` WHERE JP_Sales= 532
 -- Opdracht 8
-SELECT SUM(Global_Sales) AS verkocht_werelwijd FROM videogamesales WHERE publisher = "Nintendo";
-
+SELECT SUM(Global_Sales) FROM `videogamesales` WHERE publisher = 'Nintendo'
 -- Opdracht 9
-SELECT name, year,genre FROM videogamesales WHERE genre = "Racing" AND publisher = "Nintendo" OR publisher = "Activision";
+SELECT name , year FROM `videogamesales` WHERE genre = 'racing' AND publisher = 'Nintendo' OR publisher = 'Activision'
 -- Opdracht 10
-SELECT ROUND(AVG(NA_Sales )) AS gemiddelde_verkochte_NA,  ROUND(AVG(EU_Sales )) AS gemiddel_verkochte_EU, ROUND(AVG(JP_Sales )) AS gemiddel_verkochte_JP FROM videogamesales;
+SELECT AVG(NA_Sales) , AVG(EU_Sales) , AVG(JP_Sales) AS Gemiddelde_sales_Noord_amerika_Europa_Japan FROM `videogamesales` 
 -- Opdracht 11
-DELETE FROM videogamesales WHERE name = "Halo 2" AND platform = "XB";
+DELETE FROM `videogamesales` WHERE name = 'Halo 2'
 -- Opdracht 12
-DELETE FROM videogamesales WHERE year = 2012 OR publisher = "Ubisoft"
+DELETE FROM `videogamesales` WHERE year = 2012 OR publisher = 'ubisoft'
 -- Opdracht 13
-DELETE FROM videogamesales WHERE genre = "Adventure" AND publisher = "Nintendo";
+DELETE FROM `videogamesales` WHERE genre = 'adventure' AND publisher = 'Nintendo'
 -- Opdracht 14
-DELETE FROM videogamesales WHERE Global_Sales < 1000 AND publisher = "Nintendo";
+DELETE FROM `videogamesales` WHERE Global_Sales < 10000 AND publisher = 'Nintendo'
 -- Opdracht 15
+DELETE FROM `videogamesales` WHERE year = 1997 AND NA_Sales >= 200000
+
